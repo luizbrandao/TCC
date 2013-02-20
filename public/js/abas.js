e2d87ba16a637732755052ebb8274bd5ba447111
Luiz Brandão <luizao.cmd@gmail.com>
2013-02-20 10:05:12 -0400

    Correcoes no arquivo abas.js e adicionandoPontos.js

--- public/js/abas.js
+++ public/js/abas.js
@@ -1,73 +1,84 @@
 var $tabs = $('.tabbable li');
 $("#tab2")
-$('#prevtab').on('click', function() {
-    $tabs.filter('.active').prev('li').find('a[data-toggle="tab"]').tab('show');
-    idAtivo = $('#tab3').attr("class");
-    if(idAtivo=="tab-pane active") {
-        revisao ();
-    }
-    idAtivotb2 = $('#tab2').attr("class");
-    if(idAtivotb2=="tab-pane active") {
-        pontos();
-    }
+$('#prevtab').on(
+		'click',
+		function() {
+			$tabs.filter('.active').prev('li').find('a[data-toggle="tab"]')
+					.tab('show');
+			idAtivo = $('#tab3').attr("class");
+			if (idAtivo == "tab-pane active") {
+				revisao();
+			}
+			idAtivotb2 = $('#tab2').attr("class");
+			if (idAtivotb2 == "tab-pane active") {
+				pontos();
+			}
 
+		});
 
-});
+$('#nexttab').on(
+		'click',
+		function() {
+			$tabs.filter('.active').next('li').find('a[data-toggle="tab"]')
+					.tab('show');
 
-$('#nexttab').on('click', function() {
-    $tabs.filter('.active').next('li').find('a[data-toggle="tab"]').tab('show');
-	
-    idAtivo = $('#tab3').attr("class");
-    if(idAtivo=="tab-pane active") {
-        revisao ();
-    }
-	
-    idAtivotb2 = $('#tab2').attr("class");
-    if(idAtivotb2=="tab-pane active") {
-        pontos();
-    }
+			idAtivo = $('#tab3').attr("class");
+			if (idAtivo == "tab-pane active") {
+				revisao();
+			}
 
-});
+			idAtivotb2 = $('#tab2').attr("class");
+			if (idAtivotb2 == "tab-pane active") {
+				pontos();
+			}
+
+		});
 function pontos(id) {
-    //$("#ajax-pontos").html("");
-    $(".campo").each(function(index) {
-        ref = $(this).attr("data-ref");
-        $("#desc"+ref).html($(this).val());
-    });
+	// $("#ajax-pontos").html("");
+	$(".campo").each(function(index) {
+		ref = $(this).attr("data-ref");
+		$("#desc" + ref).html($(this).val());
+	});
 
 }
-function revisao () {
-    $("#Lista-pontos-ajax").html("");
-    $("#data-ajax").html("");
-    $("#assunto-ajax").html("");
-    $("#descricao-ajax").html("");
-    $("#presentes-ajax").html("");
+function revisao() {
+	$("#Lista-pontos-ajax").html("");
+	$("#data-ajax").html("");
+	$("#assunto-ajax").html("");
+	$("#descricao-ajax").html("");
+	$("#presentes-ajax").html("");
 
-    var data = $("#data-form").val();
-    var assunto = $("#assunto-form").val();
-    var descricao = $("#descricao-form").val();
+	var data = $("#data-form").val();
+	var assunto = $("#assunto-form").val();
+	var descricao = $("#descricao-form").val();
 
-    $("#data-ajax").html(data);
-    $("#assunto-ajax").html(assunto);
-    $("#descricao-ajax").html(descricao);
+	$("#data-ajax").html(data);
+	$("#assunto-ajax").html(assunto);
+	$("#descricao-ajax").html(descricao);
 
-    $("#presentes-ajax").html("");//Limpa o campo Pessoas Presentes
+	$("#presentes-ajax").html("");// Limpa o campo Pessoas Presentes
 
-    $("input[type=checkbox]").each(function(index) {
-        if($(this).is(':checked')) {
-            $("#presentes-ajax").append('<li>'+$(this).val()+'</li>');
+	$("input[type=checkbox]").each(
+			function(index) {
+				if ($(this).is(':checked')) {
+					// $("#presentes-ajax").append('<li>'+$(this).val()+'</li>');
+					$("#presentes-ajax").append(
+							'<div>_______________________________ <br>'
+									+ $(this).val() + '<br><div>');
+				}
+			});
 
-        }
-    });
-	
-    // Lista Pontos Ajax
-    $(".campo").each(function(index) {
-        ref = $(this).attr("data-ref");
-        idtextarea = "#pontos"+ref;
-		
-        var data = $(idtextarea).val();
-        var valorTextArea = data.replace(/\n/g,"<br>");
-		  
-        $("#Lista-pontos-ajax").append('<div class="lista-pontos"><strong>'+$(this).val()+'</strong><br>'+valorTextArea+'</div>');
-    });
+	// Lista Pontos Ajax
+	$(".campo").each(
+			function(index) {
+				ref = $(this).attr("data-ref");
+				idtextarea = "#pontos" + ref;
+
+				var data = $(idtextarea).val();
+				var valorTextArea = data.replace(/\n/g, "<br>");
+
+				$("#Lista-pontos-ajax").append(
+						'<div class="lista-pontos"><strong>' + $(this).val()
+								+ '</strong><br>' + valorTextArea + '</div>');
+			});
 }
