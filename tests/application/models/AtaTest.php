84b76a380cdd024a4916d78197e18b63c90379d5
Luiz Brandão <luizao.cmd@gmail.com>
2013-02-26 10:37:57 -0400

    Iniciando com Unit Test

---
+++ tests/application/models/AtaTest.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('/var/www/GeraAta/application/model/Ata.php');
+require_once('/var/www/GeraAta/application/modules/atas/models/Ata.php');
+
+class AtaTest extends PHPUnit_Framework_TestCase {
+	public function testAtaAppModel(){
+		$a = new Application_Model_Ata();
+		$this->assertTrue(!empty($a));
+	}
+	
+	public function testAtaModuleModel(){
+		$ata = new Atas_Model_Ata();
+		$this->assertTrue(!empty($ata));
+	}
+}
