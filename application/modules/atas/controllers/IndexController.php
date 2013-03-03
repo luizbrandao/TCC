9190369f737f8772297a23e136c5373fa6a727e0
Luiz Brandão <luizao.cmd@gmail.com>
2013-03-02 22:05:02 -0400

    Alteracao na apresentacao da Ata

--- application/modules/atas/controllers/IndexController.php
+++ application/modules/atas/controllers/IndexController.php
@@ -12,6 +12,7 @@ class Atas_IndexController extends Zend_Controller_Action
             $this->view->usuario = $SessaoUsuario;
         } else {
             $this->_helper->redirector('login', 'index', 'index');
+	}
         
     }
 
@@ -50,6 +51,7 @@ class Atas_IndexController extends Zend_Controller_Action
             $ataSalvar = new Atas_Model_Ata();
             $ataSalvar->add($ata);
             $this->_helper->redirector('index', 'index', 'atas');
+	}
         
     }
 
@@ -67,6 +69,12 @@ class Atas_IndexController extends Zend_Controller_Action
     public function updateAction()
     {
         // action body
+	$this->request = $this->getRequest();
+	$ata = new Application_Model_Ata();
+	$ata->id = $this->request->getParam('id');
+	
+	$a = new Atas_Model_Ata();
+	$this->view->ata = $a->find($ata);
     }
 
 
