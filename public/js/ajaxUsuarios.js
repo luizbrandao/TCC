402f08fdcc1326928c818acc6bd11e7ee1153f84
Thiago Elias <thiagho@gmail.com>
2013-03-05 10:00:05 -0300

    ajax teste

--- public/js/ajaxUsuarios.js
+++ public/js/ajaxUsuarios.js
@@ -6,6 +6,9 @@ jQuery.ajax({
 		for($i = 0; $i < data.length; $i++){
 			$('#usuarios').append('<label class="checkbox"><input type="checkbox" name="presentes[]"  value="'+data[$i].nome+'">'+data[$i].nome+'</label>');
 		}
+		if($("#pagina-ref")=="update") {
+			verifica_checked ();
+		}
 	},
 	error: function(){
 		alert("Não foi possivel recuperar usuarios");
